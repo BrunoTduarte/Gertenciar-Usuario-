bdb8a3621b401ff30db7946bc45deac93cc1e665
Bruno Tomé <167448778+BrunoTduarte@users.noreply.github.com>
2024-05-17 19:15:34 -0300

    Add files via upload

--- docente.js
+++ docente.js
@@ -1,1 +1,16 @@
-import User from "./user";
+import User from "./user.js";
+
+class Docente extends User {
+    constructor(nome, email, nascimento, role = 'docente', ativo = true) {
+        super(nome, email, nascimento, role, ativo)
+    }
+
+    aprovarEstudante(estudante, curso){
+        return `estudante ${estudante} aprovado no curso ${curso} com sucesso.`
+    }
+}
+
+const novoDocente = new Docente ('mariana', '@M.com', '2009-08-21')
+console.log(novoDocente)
+console.log(novoDocente.exibirInfos())
+console.log(novoDocente.aprovarEstudante('juliana', 'js'))
